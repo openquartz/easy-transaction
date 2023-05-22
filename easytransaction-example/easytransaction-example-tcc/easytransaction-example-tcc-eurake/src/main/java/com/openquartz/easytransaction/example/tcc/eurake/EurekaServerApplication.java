2ef30e1bdd8363dbbac495216b81e2015d5c404a
svnlab <svnlab>
2023-05-22 23:17:21 +0800

    add example

---
+++ easytransaction-example/easytransaction-example-tcc/easytransaction-example-tcc-eurake/src/main/java/com/openquartz/easytransaction/example/tcc/eurake/EurekaServerApplication.java
@@ -0,0 +1,25 @@
+package com.openquartz.easytransaction.example.tcc.eurake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * The type Eureka server application.
+ *
+ * @author xiaoyu
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ easytransaction-example/easytransaction-example-tcc/easytransaction-example-tcc-order/src/main/java/com/openquartz/easytransaction/example/tcc/order/OrderApplication.java
@@ -0,0 +1,12 @@
+package com.openquartz.easytransaction.example.tcc.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
