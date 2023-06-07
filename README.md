b4ab67ed04bdbe329d2e2031cfc5a468b583553f
svnlab <svnlab>
2023-06-07 22:39:41 +0800

    add sql script

--- README.md
+++ README.md
@@ -12,6 +12,26 @@
       </dependency>
 ```
 
+### 执行SQL
+```sql
+CREATE TABLE `et_transaction_certificate_entity`
+(
+    `id`                 bigint       NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `transaction_id`     varchar(50)  NOT NULL DEFAULT '' COMMENT '事务ID',
+    `certificate_status` tinyint      NOT NULL DEFAULT '0' COMMENT '凭证状态',
+    `created_time`       timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `finished_time`      datetime              DEFAULT NULL COMMENT '完成时间',
+    `updated_time`       timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+    `confirm_method`     varchar(256) NOT NULL DEFAULT '' COMMENT '取消 method',
+    `cancel_method`      varchar(256) NOT NULL DEFAULT '' COMMENT '取消 method',
+    `retry_count`        int          NOT NULL DEFAULT '0' COMMENT '重试数',
+    `version`            int          NOT NULL DEFAULT '0' COMMENT '数据版本',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_transaction_id` (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='事务凭证';
+```
+
+### 使用注解
 使用注解在try 方法上,并在相同的类下提供confirm/cancel方法
 例如：
 ```java
@@ -71,8 +91,8 @@
     }
 ```
 
-### 特性
-#### 超时&重试
+## 特性
+### 超时&重试
 同时注解支持设置事务超时时间设置以及重试设置
 ```java
 /**
--- easytransaction-repository/easytransaction-repository-jdbc/sql/TransactionCertificateEntity.sql
+++ easytransaction-repository/easytransaction-repository-jdbc/sql/TransactionCertificateEntity.sql
@@ -1,13 +1,15 @@
-CREATE TABLE et_transaction_certificate_entity(
-	id BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT 'id',
-	transaction_id VARCHAR (50) NOT NULL DEFAULT '' COMMENT '事务ID',
-	certificate_status TINYINT (3) NOT NULL DEFAULT 0 COMMENT '凭证状态',
-	created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-	finished_time DATETIME DEFAULT NULL COMMENT '完成时间',
-	updated_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-	confirm_method VARCHAR (256) NOT NULL DEFAULT '' COMMENT '取消 method',
-	cancel_method VARCHAR (256) NOT NULL DEFAULT '' COMMENT '取消 method',
-	retry_count INT (10) NOT NULL DEFAULT 0 COMMENT '重试数',
-	version INT (10) NOT NULL DEFAULT 0 COMMENT '数据版本',
-	PRIMARY KEY (id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '事务凭证';
+CREATE TABLE `et_transaction_certificate_entity`
+(
+    `id`                 bigint       NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `transaction_id`     varchar(50)  NOT NULL DEFAULT '' COMMENT '事务ID',
+    `certificate_status` tinyint      NOT NULL DEFAULT '0' COMMENT '凭证状态',
+    `created_time`       timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `finished_time`      datetime              DEFAULT NULL COMMENT '完成时间',
+    `updated_time`       timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+    `confirm_method`     varchar(256) NOT NULL DEFAULT '' COMMENT '取消 method',
+    `cancel_method`      varchar(256) NOT NULL DEFAULT '' COMMENT '取消 method',
+    `retry_count`        int          NOT NULL DEFAULT '0' COMMENT '重试数',
+    `version`            int          NOT NULL DEFAULT '0' COMMENT '数据版本',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_transaction_id` (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='事务凭证';
