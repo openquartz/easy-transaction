b73f3271528b99ad8c4ef6910f03742bd05cba4e
svnlab <svnlab>
2023-06-26 21:58:05 +0800

    add saga support

---
+++ easytransaction-core/src/main/java/com/openquartz/easytransaction/core/annotation/Saga.java
@@ -0,0 +1,35 @@
+package com.openquartz.easytransaction.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author svnee
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Saga {
+
+    /**
+     * 提交method
+     */
+    String rollbackMethod() default "";
+
+    /**
+     * 超时时间,单位：毫秒 默认不超时
+     */
+    long timeout() default Long.MAX_VALUE;
+
+    /**
+     * saga confirm method 重试次数。默认 不重试
+     */
+    int retryCount() default 0;
+
+    /**
+     * 重试时间间隔 单位：毫秒
+     */
+    long retryInterval() default 0;
+
+}
